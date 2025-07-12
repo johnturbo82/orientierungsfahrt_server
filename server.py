8ef2359d34d633dc52d4c126a81e0f7ef70549ee
Oliver Schöttner <johnturbo82@gmail.com>
2025-07-12 14:47:32 +0200

    fix

--- server.py
+++ server.py
@@ -26,6 +26,6 @@ async def get_version() -> int:
 async def get_tours() -> list[Turn]:
     t1 = Turn(id=1, name="Start", description="Start der Tour", image="start")
     t2 = Turn(id=2, name="Links abbiegen", description="Nach der Brücke", image="left")
-    t2 = Turn(id=2, name="Rechts abbiegen", image="right")
-    t3 = Turn(id=3, name="Ziel", description="Ende der Tour", image="end")
+    t2 = Turn(id=3, name="Rechts abbiegen", image="right")
+    t3 = Turn(id=4, name="Ziel", description="Ende der Tour", image="end")
     return [t1, t2, t3]
